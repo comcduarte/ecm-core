837b004d4082294f96be67c653a9befb1ad938d0
duartec <duartec@RD-WKS003.midnet.cityofmiddletown.com>
2025-09-02 12:09:02 -0400

    WIP

--- src/Metadata/src/Repository/MetadataTemplateRepository.php
+++ src/Metadata/src/Repository/MetadataTemplateRepository.php
@@ -4,106 +4,57 @@ declare(strict_types=1);
 namespace Core\Metadata\Repository;
 
 use Core\App\Repository\AbstractRepository;
-use Core\Metadata\Entity\MetadataTemplate;
+use Core\Metadata\Entity\MetadataTemplate as MetadataTemplateEntity;
 use Dot\DependencyInjection\Attribute\Entity;
+use comcduarte\Box\API\AccessToken;
 use comcduarte\Box\API\AccessTokenAwareTrait;
+use comcduarte\Box\API\Enum\FieldType;
 use comcduarte\Box\API\Exception\ClientErrorException;
 use comcduarte\Box\API\Resource\ClientError;
 use comcduarte\Box\API\Resource\Field;
+use comcduarte\Box\API\Resource\MetadataTemplate;
 use comcduarte\Box\API\Resource\MetadataTemplates;
-use comcduarte\Box\API\AccessToken;
 
-#[Entity(name: MetadataTemplate::class)]
+#[Entity(name: MetadataTemplateEntity::class)]
 class MetadataTemplateRepository extends AbstractRepository
 {
     use AccessTokenAwareTrait;
     
-    public function getMetadataTemplates(array $params,array $filters = []): MetadataTemplates
+    public function getMetadataTemplates(array $params, AccessToken $access_token): MetadataTemplates
     {
-        
-        $global = [
-            'app_folder_id'     => '335486073328',
-            'limit'             => 15,
-            'scope'             => 'enterprise_1328932288',
-        ];
-        
-        /**
-         * Set Local Variables
-         */
-//         $app_folder_id = $global['app_folder_id'];
-        
-        $metadata_template = new \comcduarte\Box\API\Resource\MetadataTemplate($params['access_token']->getAccessToken());
-        $templates = $metadata_template->list_all_metadata_templates_for_enterprise();
+        $template = new MetadataTemplate($access_token->getAccessToken());
+        $templates = $template->list_all_metadata_templates_for_enterprise();
         return $templates;
+    }
+    
+    public function createMetadataTemplate(array $params, AccessToken $access_token): void
+    {
+        $template = new MetadataTemplate($access_token->getAccessToken());
+        $template->exchangeArray($params);
+        $template->create_metadata_template();
+        return;
     }
     
     public function saveMetadataTemplate(array $params)
     {
-        $template = new \comcduarte\Box\API\Resource\MetadataTemplate($params['access_token']->getAccessToken());
+        $template = new MetadataTemplate($params['access_token']->getAccessToken());
 
-//         $template->displayName = 'contract';
-//         $template->scope = 'enterprise';
+        $template->displayName = $params['displayName'];
+        $template->templateKey = $params['displayName'];
         
-//         $array = [
-//             'COI Expiration' => 'date',
-//             'Contract Amount' => 'float',
-//             'Contract End Date' => 'date',
-//             'Contract Number' => 'string',
-//             'Contract Number Link' => 'string',
-//             'Contract Status' => 'string',
-//             'Created By' => 'string',
-//             'Department CR' => 'string',
-//             'Department Manager' => 'string',
-//             'Formal Bid Number' => 'string',
-//             'Legal Approver' => 'string',
-//             'Legal Approver Date' => 'date',
-//             'Mayors Approval' => 'string',
-//             'Mayors Approval Date' => 'date',
-//             'Pay Terms' => 'string',
-//             'Project Name' => 'string',
-//             'Purchasing Reviewer' => 'string',
-//             'Purchasing Reviewer Date' => 'date',
-//             'Renewal Term' => 'float',
-//             'Risk Approver' => 'string',
-//             'Risk Approver Date' => 'date',
-//             'Vendor Number' => 'float',
-//             'Vendor Name' => 'string',
-//         ];
-
-//         $template->displayName = 'legal-opinion';
-//         $template->scope = 'enterprise';
+        $field = new Field();
+        $field->displayName = 'testfield';
+        $field->type = FieldType::Text;
         
-//         $array = [
-//             'Book No' => 'string',
-//             'Date' => 'date',
-//             'Department' => 'string',
-//             'Doc ID' => 'string',
-//             'Opinion No' => 'string',
-//             'Subject' => 'string',
-//         ];
-        
-//         $template->displayName = 'amendment';
-//         $template->scope = 'enterprise';
-        
-//         $array = [
-//             'Amendment Number' => 'string',
-//             'Amendment Number Link' => 'string',
-//         ];
-        
-        foreach ($array as $name => $type) {
-            $x = new Field();
-            $x->displayName = $name;
-            $x->description = $name;
-            $x->type = $type;
-            
-            $template->fields[] = $x;
-        }
+        $template->fields = [
+            $field,
+        ];
         
         $result = $template->create_metadata_template();
         
         if ($result instanceof ClientError) {
             throw new ClientErrorException();
-        } elseif ($result instanceof \comcduarte\Box\API\Resource\MetadataTemplate) {
+        } elseif ($result instanceof MetadataTemplate) {
             return $result;
         }
         
@@ -111,9 +62,14 @@ class MetadataTemplateRepository extends AbstractRepository
     
     public function deleteMetadataTemplate(string $template_key, AccessToken $access_token): void
     {
-        $template = new \comcduarte\Box\API\Resource\MetadataTemplate($access_token->getAccessToken());
-        $result = $template->remove_metadata_template('enterprise', $template_key);
-        
+        $template = new MetadataTemplate($access_token->getAccessToken());
+        $template->remove_metadata_template('enterprise', $template_key);
+        return;
+    }
+
+    public function updateMetadataTemplate(MetadataTemplate $template, AccessToken $access_token): void
+    {
+        $template->update_metadata_template();
         return;
     }
 }
