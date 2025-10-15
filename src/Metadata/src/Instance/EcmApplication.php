9ba649d9d2ef8e74b6dbb34028a7e50fe83ded12
duartec <duartec@RD-WKS003.midnet.cityofmiddletown.com>
2025-10-15 15:04:35 -0400

    Create EcmApplication Instance

---
+++ src/Metadata/src/Instance/EcmApplication.php
@@ -0,0 +1,54 @@
+<?php
+declare(strict_types=1);
+
+namespace Core\Metadata\Instance;
+
+use comcduarte\Box\API\Resource\MetadataInstance;
+
+class EcmApplication extends MetadataInstance
+{
+    protected string $queue;
+    protected string $contractNumber;
+    
+    public function getQueue()
+    {
+        return $this->queue;
+    }
+    
+    public function setQueue($queue)
+    {
+        $this->queue = $queue;
+        return $this;
+    }
+    
+    public function getContractnumber()
+    {
+        return $this->contractNumber;
+    }
+    
+    public function setContractnumber($contractNumber)
+    {
+        $this->contractNumber = $contractNumber;
+        return $this;
+    }
+    
+    public function exchangeArray($data)
+    {
+        parent::exchangeArray($data);
+        
+        $this->contractNumber = $data['contractNumber'] ?? null;
+        $this->queue = $data['queue'] ?? null;
+    }
+    
+    public function getArrayCopy()
+    {
+        
+        
+        $data = parent::getArrayCopy();
+
+        $data['queue'] = $this->queue;
+        $data['contractNumber'] = $this->contractNumber;
+        
+        return $data;
+    }
+}
