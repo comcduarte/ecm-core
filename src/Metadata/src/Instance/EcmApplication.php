a076c4514289b659f53ad63c766c804af847ccda
duartec <duartec@RD-WKS003.midnet.cityofmiddletown.com>
2025-11-07 10:29:13 -0500

    WIP

--- src/Metadata/src/Instance/EcmApplication.php
+++ src/Metadata/src/Instance/EcmApplication.php
@@ -9,6 +9,7 @@ class EcmApplication extends MetadataInstance
 {
     protected string $queue = '';
     protected string $contractNumber = '';
+    protected string $projectName = '';
     
     public function getQueue()
     {
@@ -18,6 +19,17 @@ class EcmApplication extends MetadataInstance
     public function setQueue($queue)
     {
         $this->queue = $queue;
+        return $this;
+    }
+    
+    public function getProjectName(): string
+    {
+        return $this->projectName;
+    }
+    
+    public function setProjectName($projectName): self
+    {
+        $this->projectName = $projectName;
         return $this;
     }
     
@@ -36,8 +48,9 @@ class EcmApplication extends MetadataInstance
     {
         parent::exchangeArray($data);
         
-        $this->contractNumber = $data['contractNumber'] ?? null;
-        $this->queue = $data['queue'] ?? null;
+        $this->contractNumber = $data['contract-number'] ?? "";
+        $this->queue = $data['queue'] ?? "";
+        $this->projectName = $data['project-name'] ?? "";
     }
     
     public function getArrayCopy()
