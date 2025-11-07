dd5b653d4976ea20c208e86ac022c1f06c4da132
duartec <duartec@RD-WKS003.midnet.cityofmiddletown.com>
2025-11-07 10:29:39 -0500

    Always include CSRF

--- src/App/src/InputFilter/AbstractInputFilter.php
+++ src/App/src/InputFilter/AbstractInputFilter.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace Core\App\InputFilter;
 
+use Frontend\App\InputFilter\Input\CsrfInput;
 use Laminas\InputFilter\InputFilter;
 
 /**
@@ -12,4 +13,9 @@ use Laminas\InputFilter\InputFilter;
  */
 abstract class AbstractInputFilter extends InputFilter
 {
+    public function init(): self
+    {
+        return $this
+        ->add(new CsrfInput('createContractCsrf', false));
+    }
 }
