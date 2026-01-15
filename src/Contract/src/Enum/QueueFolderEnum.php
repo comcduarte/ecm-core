84bcc3b7a8bcbe2cf555d62e77ee8b6a0d73252d
duartec <duartec@RD-WKS003.midnet.cityofmiddletown.com>
2026-01-15 16:14:31 -0500

    Commit Current Production Values

--- src/Contract/src/Enum/QueueFolderEnum.php
+++ src/Contract/src/Enum/QueueFolderEnum.php
@@ -10,58 +10,53 @@ Enum QueueFolderEnum: string
      * under the word production, and remove the space between under development, and vice versa.
      */
     
-    /**
-     * Production
-     * /
-    case ECM_ONBASE = "348908106622";
-    case ECM_TEMPLATES = "348908147055";
-    
-    case ECM_AC = "352155187097";
-    case ECM_BD = "352158683991";
-case ECM_BE = "352589456548";
-    case ECM_CC = "352158515242";
-    case ECM_CN = "352156775699";
-    case ECM_ED = "352158738093";
+    /*** Production ***/
+    case AC = "352155187097";
+    case BD = "352158683991";
+    case BE = "352589456548";
+    case CC = "352158515242";
+    case CDBG = "352587578019";
+    case CN = "352156775699";
     case ECM_EG = "352157531051";
-case ECM_EM = "352587865757";
-    case ECM_EO = "352153422658";
-    case ECM_FD = "352155414876";
-    case ECM_FN = "352157027667";
-    case ECM_GC = "352154224263";
-case ECM_HD = "352587945272";
-case ECM_HR = "352386616195";
-case ECM_IT = "352587776524";
-case ECM_LU = "352587489095";
-    case ECM_MO = "352156826206";
-    case ECM_PA = "352155597129";
-case ECM_PD = "352589142863";
-    case ECM_PK = "352155378264";
-    case ECM_PU = "352157774369";
-case ECM_PW = "352588405548";
-    case ECM_PY = "352157135066";
-    case ECM_PZ = "352155522074";
-    case ECM_RC = "352153609698";
-    case ECM_RL = "352153854647";
-    case ECM_RM = "352158656478";
-    case ECM_RV = "352153619298";
-    case ECM_SC = "352155160408";
-    case ECM_SD = "352157783941";
-    case ECM_TA = "352154416558";
-    case ECM_TC = "352155030565";
-    case ECM_TX = "352153976896";
-    case ECM_WS = "352158966870";
-    case ECM_YS = "352156770556";
+    case ECM_IT = "352587776524";
+    case ED = "352158738093";
+    case EM = "352587865757";
+    case EO = "352153422658";
+    case FD = "352155414876";
+    case FN = "352157027667";
+    case GC = "352154224263";
+    case HD = "352587945272";
+    case HR = "352386616195";
+    case LU = "352587489095";
+    case MO = "352156826206";
+    case PA = "352155597129";
+    case PD = "352589142863";
+    case PK = "352155378264";
+    case PU = "352157774369";
+    case PW = "352588405548";
+    case PY = "352157135066";
+    case PZ = "352588931908";
+    case RC = "352153609698";
+    case RL = "352153854647";
+    case RM = "352158656478";
+    case RV = "352588525676";
+    case SC = "352155160408";
+    case SD = "352157783941";
+    case TA = "352154416558";
+    case TC = "352155030565";
+    case TX = "352153976896";
+    case WS = "352587714239";
+    case YS = "352589278948";
     
-    case ECM_DEPARTMENTS = "348908902955";
-    case ECM_LEGAL = "348907511883";
-    case ECM_MAYOR = "348911714273";
-    case ECM_PURCHASING = "348907826208";
-    case ECM_RISK = "348908361510";
-    case ECM_VENDOR = "348910710012";
+    case DEPARTMENTS = "348908902955";
+    case LEGAL = "348907511883";
+    case MAYOR = "348911714273";
+    case PURCHASING = "348907826208";
+    case RISK = "348908361510";
+    case VENDOR = "348910710012";
     
-    /**
-     * Development
-     */
+    
+    /*** Development *** /
     case ECM_ONBASE = "336171280120";
     case ECM_TEMPLATES = "343236246817";
     
@@ -107,9 +102,7 @@ case ECM_PW = "352588405548";
     case ECM_RISK = "336171945517";
     case ECM_VENDOR = "345199902550";
     
-    /**
-     * End
-     */
+    /*** End ***/
     
     /**
      * @return non-empty-string[]
